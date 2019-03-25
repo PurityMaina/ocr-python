71bf89db7db46a0caabf92b1628cce9ac5521640
purity <pmaina@lipalater.com>
2019-03-25 13:09:48 +0300

    Initial commit

---
+++ ocr/handler.py
@@ -0,0 +1,24 @@
+import json
+import pytesseract
+import PIL.Image
+import io
+import os
+import json
+from base64 import b64decode
+
+
+def main(event, context):
+  print("Event Passed to Handler: " + json.dumps(event))
+  image_base64 = json.loads(event['body'])['image64']
+  binary = b64decode(image_base64)
+  image = PIL.Image.open(io.BytesIO(binary))
+  text = pytesseract.image_to_string(image, config='--psm 6')
+
+  message = {
+       'test': text
+    }
+  return {
+      'statusCode': 200,
+      'headers': {'Content-Type': 'application/json'},
+      'body': json.dumps(message)
+  }
